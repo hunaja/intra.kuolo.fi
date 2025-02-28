48ab83ab38858271d87aa99b996a6be1b41af762
elias <79339968+hunaja@users.noreply.github.com>
2025-02-28 22:25:16 +0200

    Remove clerk

--- src/components/index/inauthenticatedPage.tsx
+++ src/components/index/inauthenticatedPage.tsx
@@ -1,9 +1,7 @@
 "use client";
 
-import { Button } from "@nextui-org/button";
-import { SignInButton } from "@clerk/nextjs";
-import { Link } from "@nextui-org/react";
 import NavigationBar from "../navigation/navigation";
+import StyledSignOutButton from "../signOut/button";
 
 export default function InauthenticatedPage() {
   return (
@@ -13,14 +11,7 @@ export default function InauthenticatedPage() {
       <main className="block min-h-full flex-1 flex-row justify-between sm:flex sm:px-10">
         <div className="align-center flex w-full flex-1 flex-col place-items-center justify-center sm:w-96">
           <h1 className={`mb-10 text-center text-3xl`}>Jäsensivut</h1>
-          <SignInButton>
-            <Button color="primary" className="">
-              Kirjaudu sisään
-            </Button>
-          </SignInButton>
-          <p className="mt-5 text-sm">
-            <Link href="/get-invited">Luo tunnus</Link>
-          </p>
+          <StyledSignOutButton />
         </div>
       </main>
     </>
